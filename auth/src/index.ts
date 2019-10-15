5e8ad7f004744ed9a5fd887c5cacc6ede0f1db01
Brian Emilius <brian@brianemilius.com>
2019-10-15 21:56:12 +0200

    git add simple index to get started

---
+++ auth/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express"
+const app = express()
+const router = express.Router()
+
+router.get("/", function(req, res) {
+	res.send("Hello, World!");
+});
+
+app.use(router);
+
+app.listen(3000);
