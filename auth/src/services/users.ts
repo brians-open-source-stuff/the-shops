974630e0c09a556ec53c8f6592d35fccdcce1637
Brian Emilius <brian@brianemilius.com>
2019-10-16 18:06:17 +0200

    add user service

--- auth/src/services/users.ts
+++ auth/src/services/users.ts
@@ -1,8 +1,10 @@
 import User from "../models/user.model"
+import { compare } from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 export const findUserByUsername = async function(
 	username: string
-): Promise<void> {
+): Promise<any> {
 	try {
 		Promise.resolve(
 			await User.findOne({
@@ -14,4 +16,26 @@ export const findUserByUsername = async function(
 	} catch (error) {
 		Promise.reject(error)
 	}
+}
+
+export const verifyUser = async function(
+	user: any,
+	password: string
+): Promise<any> {
+	try {
+		Promise.resolve(await compare(password, user.password))
+	} catch (error) {
+		Promise.reject(error)
+	}
+}
+
+export const createToken = async function(user: any): Promise<any> {
+	jwt.sign(
+		user,
+		process.env.TOKEN_SECRET || "secret keyboard catfish",
+		function(err: Error, token: any) {
+			if (err) return Promise.reject(err)
+			Promise.resolve(token)
+		}
+	)
 }
