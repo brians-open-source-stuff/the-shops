3ff9ff63f6c9fccf45be8961763ae2ca3439800b
Brian Emilius <brian@brianemilius.com>
2019-10-16 16:15:30 +0200

    add statuscode to response

--- auth/src/routes/home.route.ts
+++ auth/src/routes/home.route.ts
@@ -3,6 +3,7 @@ import e from "express"
 module.exports = function(router: e.Router): void {
 	router.get("/", function(req, res): void {
 		res.json({
+			status: 200,
 			message: "Hello, World!"
 		})
 	})
