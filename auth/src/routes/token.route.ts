840f9bbf2554da7cc6477f774a98bb48f9944ea6
Brian Emilius <brian@brianemilius.com>
2019-10-16 16:15:54 +0200

    remove GET, add POST to OPTIONS

--- auth/src/routes/token.route.ts
+++ auth/src/routes/token.route.ts
@@ -2,7 +2,7 @@ import e from "express"
 
 module.exports = function(router: e.Router): void {
 	router.options("/token", function(req, res): void {
-		res.header("Allow", "OPTIONS, GET")
+		res.header("Allow", "OPTIONS, POST")
 		res.status(204)
 		res.end()
 	})
