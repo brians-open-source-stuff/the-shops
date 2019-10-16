2d8155604436661b3b8e289a68217400fca91bab
Brian Emilius <brian@brianemilius.com>
2019-10-16 17:03:45 +0200

    add .env support

--- auth/src/config/database.ts
+++ auth/src/config/database.ts
@@ -1,5 +1,10 @@
 import { Sequelize } from "sequelize"
 
-export default new Sequelize("", "", "", {
-	dialect: "mysql"
-})
+export default new Sequelize(
+	process.env.DB_NAME || "the_shops",
+	process.env.DB_USER || "root",
+	process.env.DB_PASSWORD || "",
+	{
+		dialect: "mysql"
+	}
+)
