6f5c6f4b763c9b08c500cd33528f2e107c32dc8f
Brian Emilius <brian@brianemilius.com>
2019-10-16 16:45:05 +0200

    fix log bug

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -3,7 +3,7 @@ import e from "express"
 export default async function(app: e.Application, logger: any): Promise<void> {
 	try {
 		const server = app.listen(parseInt(process.argv[2]) || 3000) as any
-		logger.info("App is listening on port", server.address().port)
+		logger.info(`App is listening on port ${server.address().port}`)
 	} catch (error) {
 		logger.error(error.stack)
 		process.exit(1)
