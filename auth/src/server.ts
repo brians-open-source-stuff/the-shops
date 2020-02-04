6d3d0488e43d286b04d321103d3cc952634fe674
Brian Emilius <BrianEmilius@users.noreply.github.com>
2020-02-04 19:07:13 +0100

    App server (#3)
    * add error logging to token controller
    
    * add cors package
    
    * add app-server destructered flow

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -1,11 +1,18 @@
-import e from "express"
+#!/usr/bin/env node
+import app from "./index"
+import { createServer as http } from "http"
+import { createServer as https } from "https"
+import { logger } from "./middleware/logging"
 
-export default async function(app: e.Application, logger: any): Promise<void> {
-	try {
-		const server = app.listen(parseInt(process.argv[2]) || 3000) as any
-		logger.info(`App is listening on port ${server.address().port}`)
-	} catch (error) {
-		logger.error(error.stack)
-		process.exit(1)
-	}
+const server =
+	process.env.NODE_ENV == "production"
+		? (https(app) as any)
+		: (http(app) as any)
+
+server.listen(process.env.PORT || 3001)
+
+function listenHandler(): void {
+	logger.info(`Application is listening on port ${server.address().port}`)
 }
+
+server.on("listening", listenHandler)
